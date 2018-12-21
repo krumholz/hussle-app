c1d6f1aea1808e412b230b4ee210ccfea6e5f77f
Nick Krumholz <nick.krumholz@gmail.com>
2018-12-21 10:01:05 -0800

    Database test working locally. Pushing to production to verify that works as well.

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+db_user = os.environ['CLOUD_SQL_USERNAME']
+db_password = os.environ['CLOUD_SQL_PASSWORD']
+db_name = os.environ['CLOUD_SQL_DATABASE_NAME']
+db_connection_name = os.environ['CLOUD_SQL_CONNECTION_NAME']
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
 
 @app.route("/")
 def hello():
     return render_template('layout.html')
 
+@app.route('/db')
+def main():
+    # When deployed to App Engine, the `GAE_ENV` environment variable will be
+    # set to `standard`
+    if os.environ.get('GAE_ENV') == 'standard':
+        # If deployed, use the local socket interface for accessing Cloud SQL
+        unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              unix_socket=unix_socket, db=db_name)
+    else:
+        # If running locally, use the TCP connections instead
+        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+        # so that your application can use 127.0.0.1:3306 to connect to your
+        # Cloud SQL instance
+        host = '127.0.0.1'
+        cnx = pymysql.connect(user=db_user, password=db_password,
+                              host=host, db=db_name)
+
+    with cnx.cursor() as cursor:
+        cursor.execute('SELECT NOW() as now;')
+        result = cursor.fetchall()
+        current_time = result[0][0]
+    cnx.close()
+
+    return str(current_time)
+# [END gae_python37_cloudsql_mysql]
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
